f00f680081d63858470de9f1997688d4147fe0e4
Scott Chen <scott@ezmail.com.tw>
2023-01-14 23:40:56 +0800

    1.5 - Refactoring for Speed

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -15,15 +15,30 @@ class ConcertTest extends TestCase
     /** @test */
     function can_get_formatted_date()
     {
-        // Create a concert with a known date
-        $concert = Concert::factory()->create([
+        $concert = Concert::factory()->make([
             'date' => Carbon::parse('2016-12-01 8:00pm'),
         ]);
 
-        // Retrieve the formatted date
-        $date = $concert->formatted_date;
+        $this->assertEquals('December 1, 2016', $concert->formatted_date);
+    }
 
-        // Verify the date is formatted as expected
-        $this->assertEquals('December 1, 2016', $date);
+    /** @test */
+    function can_get_formatted_start_time()
+    {
+        $concert = Concert::factory()->make([
+            'date' => Carbon::parse('2016-12-01 17:00:00'),
+        ]);
+
+        $this->assertEquals('5:00pm', $concert->formatted_start_time);
+    }
+
+    /** @test */
+    function can_get_ticket_price_in_dollars()
+    {
+        $concert = Concert::factory()->make([
+            'ticket_price' => 6750,
+        ]);
+
+        $this->assertEquals('67.50', $concert->ticket_price_in_dollars);
     }
 }
